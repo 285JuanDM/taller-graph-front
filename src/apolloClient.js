493fd521408643c5ea3797ae7a8543439bd51a28
juanMS <285juanda@gmail.com>
2025-10-30 19:36:17 -0500

    Se cre variable de entorno para la URL del back

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,8 +1,8 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const link = new HttpLink({
-    uri: "https://backedn-graphql.onrender.com/graphql"
-})
+    uri: import.meta.env.VITE_GRAPHQL_URL
+});
 
 export const client = new ApolloClient({
     link,
