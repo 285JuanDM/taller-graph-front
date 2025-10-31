018f11d6251cb3b9bba8daa23944ad79935c8f1e
juanMS <285juanda@gmail.com>
2025-10-30 20:03:53 -0500

    Se cambia importación del clien de apollo para que no empiece con mayuscula

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { ApolloProvider } from '@apollo/client/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { client } from './ApolloClient.js'
+import { client } from './apolloClient.js'
 import App from './App.jsx'
 import './index.css'
 
